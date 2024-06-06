f56a769f60ef47ca902767230f0177b8daa21257
eswar reddy <erdata2005@outlook.com>
2024-06-06 18:30:47 +0530

    every thing is upto date

--- linked-list/singlyLL.py
+++ linked-list/singlyLL.py
@@ -41,7 +41,7 @@ class Linked_List:
             while(current!=None):
                   print(current.data,"--> ",end="")
                   current=current.next
-            print("NONE",end="")
+            print("NONE")
 
       def deleteNode(self, key): 
         current = self.head
@@ -66,8 +66,52 @@ class Linked_List:
         prev.next = current.next
  
         current=None
-  
-            
+      def i_search(self,key):
+            current=self.head
+            if self.head==key:
+                  return "head is the element\n"
+            else:
+                  while(current.next!=None and current.data!=key):
+                        current=current.next
+                  if current.data==key:
+                        return "element is in this linked list\n"
+                  else:
+                        return "element not found"
+      def r_search(self,ln,key):
+            # Base case ln = none means we have reached the end of the linked list 
+            if ln==None:
+                  return "element not found"
+            # If key is present in
+            # current node, return elem found
+            if ln.data==key:
+                  return "element is in this linked list"
+            #if the key is not found in the current node, the function calls itself recursively with the next node (ln.next) and the same key. This continues the search in the remaining part of the linked list.
+            return self.r_search(ln.next,key)
+      
+      def i_length(self):
+            current=self.head
+            count=0 # intialise count to zero 
+            while(current!=None):# traversal of entire linked list
+                  count+=1 #increment while leaving form a node 
+                  current=current.next # moving to next node 
+            return count
+      
+      def r_length(self,ln,count):
+            #base case it means we have travelled  entire list
+            if ln == None:
+                  return count
+            else:
+                  count+=1
+                  return self.r_length(ln.next,count)
+      def reverseLL(self):
+            prev=None
+            current=self.head
+            while(current!=None):
+                  next=current.next
+                  current.next=prev
+                  prev=current
+                  current=next
+            self.head=prev     
 
 n1=Node(1)
 n2=Node(3)
@@ -81,13 +125,12 @@ LL.insertion_at_end(n3)
 LL.insertion_at_end(n4)
 LL.insertion_at_end(n5)
 LL.insertion_at_end(n6)
-# n7=Node(90)
-# LL.insertion_at_index(n7,5)
-LL.deleteNode(9)
+LL.reverseLL()
 LL.display()
+
+
                   
-                  
-                  
+               
                   
 
 
