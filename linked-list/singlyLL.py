d9231ee7111762cee9db08fcd1147cf21cbbdb07
eswar reddy <erdata2005@outlook.com>
2024-06-06 17:27:04 +0530

    every thing upto date

--- linked-list/singlyLL.py
+++ linked-list/singlyLL.py
@@ -13,29 +13,29 @@ class Linked_List:
             self.head=node
             self.next=None
       def insertion_at_end(self,new_node):
-            if self.head==None:
+            if self.head==None: #if head is None then return string 
                   return 'memory allocation failed'
-            elif self.head!=None:
+            elif self.head!=None:# else if head!=None then traverse linked list till current reaches the last 
                   current=self.head
                   while(current.next!=None):
                         current=current.next
                   new_node.next=None
                   current.next=new_node
-            return self.head
+            
       def insertion_at_beginning(self,new_node):
             if self.head==None:
                   self.head=new_node
-            else:
+            else:#link newnode to head first then assign head to newnode
                   new_node.next=self.head
                   self.head=new_node
-            return self.head
+
       def insertion_at_index(self,new_node,index):
             ptr=self.head
             for i in range(0,index-1):
                   ptr=ptr.next
             new_node.next=ptr.next
             ptr.next=new_node
-            return self.head
+            
       def display(self):
             current=self.head
             while(current!=None):
@@ -43,6 +43,31 @@ class Linked_List:
                   current=current.next
             print("NONE",end="")
 
+      def deleteNode(self, key): 
+        current = self.head
+        # If head node itself holds the key to be deleted 
+        if current is not None and self.head.data==key:
+                self.head = current.next
+                current = None
+                return
+        # Search for the key to be deleted, keep track of the 
+        # previous node as we need to change 'prev.next' 
+        while(current.next!=None): 
+            if current.data == key: 
+                break
+            prev = current 
+            current = current.next
+ 
+        # if key was not present in linked list 
+        if(current==None): 
+            return
+ 
+        # Unlink the node from linked list 
+        prev.next = current.next
+ 
+        current=None
+  
+            
 
 n1=Node(1)
 n2=Node(3)
@@ -56,10 +81,10 @@ LL.insertion_at_end(n3)
 LL.insertion_at_end(n4)
 LL.insertion_at_end(n5)
 LL.insertion_at_end(n6)
-n7=Node(90)
-LL.insertion_at_index(n7,5)
+# n7=Node(90)
+# LL.insertion_at_index(n7,5)
+LL.deleteNode(9)
 LL.display()
-            
                   
                   
                   
