7dc2382b7c3389f56bf8142d302bc0d556e4faf8
eswar reddy <erdata2005@outlook.com>
2024-06-19 11:11:47 +0530

    everything is upto date

--- linked-list/circularLL.py
+++ linked-list/circularLL.py
@@ -6,7 +6,6 @@ class Node:
 class CircularLinkedList:
     def __init__(self):
         self.head = None
-        
 
     def append(self, data):
         new_node = Node(data)
@@ -14,10 +13,10 @@ class CircularLinkedList:
             self.head = new_node
             self.head.next = self.head
         else:
-            current = self.head
-            while current.next != self.head:
-                current = current.next
-            current.next = new_node
+            temp = self.head
+            while temp.next != self.head:
+                temp = temp.next
+            temp.next = new_node
             new_node.next = self.head
 
     def prepend(self, data):
@@ -26,12 +25,25 @@ class CircularLinkedList:
             self.head = new_node
             self.head.next = self.head
         else:
-            current = self.head
-            while current.next != self.head:
-                current = current.next
-            current.next = new_node
             new_node.next = self.head
+            temp = self.head
+            while temp.next != self.head:
+                temp = temp.next
+            temp.next = new_node
             self.head = new_node
+
+    def display(self):
+        if not self.head:
+            print("List is empty.")
+            return
+        temp = self.head
+        nodes = []
+        while True:
+            nodes.append(temp.data)
+            temp = temp.next
+            if temp == self.head:
+                break
+        print("Circular Linked List:", " -> ".join(map(str, nodes)), "(head -> ... -> head)")
 
     def delete(self, key):
         if self.head:
@@ -39,85 +51,41 @@ class CircularLinkedList:
                 if self.head.next == self.head:
                     self.head = None
                 else:
-                    current = self.head
-                    while current.next != self.head:
-                        current = current.next
-                    current.next = self.head.next
+                    temp = self.head
+                    while temp.next != self.head:
+                        temp = temp.next
+                    temp.next = self.head.next
                     self.head = self.head.next
             else:
-                current = self.head
+                temp = self.head
                 prev = None
-                while current.next != self.head:
-                    if current.data == key:
-                        prev.next = current.next
-                        return
-                    prev = current
-                    current = current.next
-                if current.data == key:
-                    prev.next = current.next
+                while temp.next != self.head:
+                    prev = temp
+                    temp = temp.next
+                    if temp.data == key:
+                        prev.next = temp.next
+                        temp = temp.next
 
-    def display(self):
-        nodes = []
-        if self.head:
-            current = self.head
-            while True:
-                nodes.append(current.data)
-                current = current.next
-                if current == self.head:
-                    break
-        return nodes
-
-    def insert_after(self, prev_node_data, data):
-        if self.head:
-            current = self.head
-            while True:
-                if current.data == prev_node_data:
-                    new_node = Node(data)
-                    new_node.next = current.next
-                    current.next = new_node
-                    return
-                current = current.next
-                if current == self.head:
-                    break
-
-    def insert_before(self, next_node_data, data):
-        if self.head:
-            current = self.head
-            prev = None
-            while True:
-                if current.data == next_node_data:
-                    new_node = Node(data)
-                    new_node.next = current
-                    if prev:
-                        prev.next = new_node
-                    else:
-                        # Find the last node to update its next pointer
-                        last = self.head
-                        while last.next != self.head:
-                            last = last.next
-                        last.next = new_node
-                        self.head = new_node
-                    return
-                prev = current
-                current = current.next
-                if current == self.head:
-                    break
+    def __len__(self):
+        count = 0
+        if not self.head:
+            return count
+        temp = self.head
+        while True:
+            count += 1
+            temp = temp.next
+            if temp == self.head:
+                break
+        return count
 
 # Example usage:
 cll = CircularLinkedList()
 cll.append(1)
 cll.append(2)
 cll.append(3)
-print("Circular Linked List:", cll.display())  # [1, 2, 3]
-
+cll.display()  # Output: Circular Linked List: 1 -> 2 -> 3 (head -> ... -> head)
 cll.prepend(0)
-print("After prepend:", cll.display())  # [0, 1, 2, 3]
-
-cll.insert_after(2, 2.5)
-print("After inserting 2.5 after 2:", cll.display())  # [0, 1, 2, 2.5, 3]
-
-cll.insert_before(2, 1.5)
-print("After inserting 1.5 before 2:", cll.display())  # [0, 1, 1.5, 2, 2.5, 3]
-
-cll.delete(1.5)
-print("After deleting 1.5:", cll.display())  # [0, 1, 2, 2.5, 3]
+cll.display()  # Output: Circular Linked List: 0 -> 1 -> 2 -> 3 (head -> ... -> head)
+cll.delete(2)
+cll.display()  # Output: Circular Linked List: 0 -> 1 -> 3 (head -> ... -> head)
+print(f"Length of list: {len(cll)}")   # Output: Length of list: 3
---
+++ linked-list/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+ def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.head.next = self.head
+        else:
+            current = self.head
+            while current.next != self.head:
+                current = current.next
