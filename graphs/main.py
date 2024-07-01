f9710101189eba2a0287cb2195922755e378e194
eswar reddy <erdata2005@outlook.com>
2024-07-01 10:47:51 +0530

    everything is upto date

--- graphs/main.py
+++ graphs/main.py
@@ -127,10 +127,222 @@ def BFSListPath(AList,start_vertex):
     return(visited,parent)
 
 
-AList ={0: [1, 2], 1: [3, 4], 2: [4, 3], 3: [4], 4: []}
-print(BFSListPath(AList,2))
+#Find level number of vertices using BFS
+#Maintain level information to record length of the shortest path, in terms of number of edges or vertex.
+# Using BFS approch For Adjacency list, for path, maintaining the parent of each vertex
+# Using BFS approch maintaing the adjacent level number from source vertrex
+def BFSListPathLevel(AList,v):
+    # Initialization
+    (level,parent) = ({},{})
+    for each_vertex in AList.keys():
+        level[each_vertex] = -1
+        parent[each_vertex] = -1
+    
+    # Create Queue object q
+    q = Queue()
+    
+    # Assigning the level 0 for start_vertex and insert it into the queue
+    level[v] = 0
+    q.enqueue(v)
+    
+    # Repeat the following until the queue is empty
+    while(not q.isempty()):
+        # Remove the one vertex from queue
+        curr_vertex = q.dequeue()
+        # Visit the each adjacent of curr_vertex(if level value is -1) and insert into the queue
+        for adj_vertex in AList[curr_vertex]:
+            if (level[adj_vertex] == -1):
+                # Assign the level value on each adjacent one more than the curr_vertex level
+                level[adj_vertex] = level[curr_vertex] + 1
+                # Assigne the curr_vertex as parent of adjacent vertex of curr_vertex
+                parent[adj_vertex] = curr_vertex
+                q.enqueue(adj_vertex)
+    return(level,parent)
+
+# Using BFS approch For Adjacency list, for path, maintaining the parent of each vertex
+# Using BFS approch maintaing the adjacent level number from source vertrex
+def BFSListPathLevel(AList,v):
+    # Initialization
+    (level,parent) = ({},{})
+    for each_vertex in AList.keys():
+        level[each_vertex] = -1
+        parent[each_vertex] = -1
+    
+    # Create Queue object q
+    q = Queue()
+    
+    # Assigning the level 0 for start_vertex and insert it into the queue
+    level[v] = 0
+    q.enqueue(v)
+    
+    # Repeat the following until the queue is empty
+    while(not q.isempty()):
+        # Remove the one vertex from queue
+        curr_vertex = q.dequeue()
+        # Visit the each adjacent of curr_vertex(if level value is -1) and insert into the queue
+        for adj_vertex in AList[curr_vertex]:
+            if (level[adj_vertex] == -1):
+                # Assign the level value on each adjacent one more than the curr_vertex level
+                level[adj_vertex] = level[curr_vertex] + 1
+                # Assigne the curr_vertex as parent of adjacent vertex of curr_vertex
+                parent[adj_vertex] = curr_vertex
+                q.enqueue(adj_vertex)
+                
+    return(level,parent)
+
+#DFS Recursive (without using external stack) 
+# Initialization Function
+def DFSInitList(AList):
+    (visited,parent) = ({},{})
+    for each_vertex in AList.keys():
+        visited[each_vertex] = False
+        parent[each_vertex] = -1
+    return(visited,parent)
 
 
+# DFS Recursive Implementation for Adjacency list
+def DFSList(AList,visited,parent,v):
+    # Mark vertex v as visited vertex
+    visited[v] = True
+    # Repeat following for each unvisited adjacent of vertex v
+    for adj_vertex in AList[v]:
+        if (not visited[adj_vertex]):
+            # Assign vertex v as parent of each unvisited adjacent of v 
+            parent[adj_vertex] = v
+            
+            # Recursively call the DFS on unvisited adjacent of v
+            (visited,parent) = DFSList(AList,visited,parent,adj_vertex)
+            
+    return(visited,parent)
+
+# Global variable
+(visited,parent) = ({},{})
+
+# Initialization function
+def DFSInitListGlobal(AList):   
+    for each_vertex in AList.keys():
+        visited[each_vertex] = False
+        parent[each_vertex] = -1
+    return
+
+# DFS Recursive Implementation for Adjacency list
+def DFSListGlobal(AList,v):
+    # Mark vertex v as visited vertex
+    visited[v] = True
+    # Repeat following for each unvisited adjacent of vertex v
+    for adj_vertex in AList[v]:
+        if (not visited[adj_vertex]):
+            # Assign vertex v as parent of each unvisited adjacent of v 
+            parent[adj_vertex] = v
+            # Recursively call the DFS on unvisited adjacent of v
+            DFSListGlobal(AList,adj_vertex)                
+    return
+
+
+#DFS global for adjacency matrix of graph
+# Function to return list of neighbours or adjacent vertex of vertex i
+# def neighbours(AMat,i):
+#     nbrs = []
+#     (rows,cols) = AMat.shape
+#     for j in range(cols):
+#         if AMat[i,j] == 1:
+#             nbrs.append(j)
+#     return(nbrs)
+
+# # Initialization Function
+# def DFSInitGlobal(AMat):
+#     (rows,cols) = AMat.shape    
+#     for each_vertex in range(rows):
+#         visited[each_vertex] = False
+#         parent[each_vertex] = -1
+#     return
+
+# # DFS Recursive Implementation for Adjacency matrix
+# def DFSGlobal(AMat,v):
+#     # Mark vertex v as visited vertex
+#     visited[v] = True
+#     # Repeat following for each unvisited adjacent of vertex v
+#     for adj_vertex in neighbours(AMat,v):
+#         if (not visited[adj_vertex]):
+#             # Assign vertex v as parent of each unvisited adjacent of v
+#             parent[adj_vertex] = v
+#             # Recursively call the DFS on unvisited adjacent of v
+#             DFSGlobal(AMat,adj_vertex)                
+#     return
+
+# Implementation of Topological sort for Adjacency list
+def toposortlist(AList):
+    # Initialization
+    (indegree,toposortlist) = ({},[])
+    zerodegreeq = Queue()
+    for u in AList.keys():
+        indegree[u] = 0
+    
+    # Compute indegree for each vertex
+    for u in AList.keys():
+        for v in AList[u]:
+            indegree[v] = indegree[v] + 1
+    
+    # Find the vertex with indegree 0 and added into the queue
+    for u in AList.keys():
+        if indegree[u] == 0:
+            zerodegreeq.enqueue(u)
+    
+    # Topological sort Computing process
+    while (not zerodegreeq.isempty()):
+        # Remove one vertex from queue which have zero degree vertices
+        curr_vertex = zerodegreeq.dequeue()       
+        # Store the removed vertex in toposortlist and reduce the indegree by one 
+        toposortlist.append(curr_vertex)
+        indegree[curr_vertex] = indegree[curr_vertex]-1
+        
+        # Repeat for each adjacent of the removed vertex
+        for adj_vertex in AList[curr_vertex]:
+            # Reduce the indegree of each adjacent of the removed vertex by 1
+            indegree[adj_vertex] = indegree[adj_vertex] - 1
+            # If after reducing the degree of adjacent, it becomes zero then insert it into the queue
+            if indegree[adj_vertex] == 0:
+                zerodegreeq.enqueue(adj_vertex)                
+    
+    return(toposortlist)
+
+
+
+# Implementation Longest path on DAG for Adjacency list
+def longestpathlist(AList):
+    # Initialization
+    (indegree,lpath) = ({},{})
+    zerodegreeq = Queue()
+    for u in AList.keys():
+        (indegree[u],lpath[u]) = (0,0)
+    
+    # Compute indegree for each vertex
+    for u in AList.keys():
+        for v in AList[u]:
+            indegree[v] = indegree[v] + 1
+
+    # Find the vertex with indegree 0 and added into the queue
+    for u in AList.keys():
+        if indegree[u] == 0:
+            zerodegreeq.enqueue(u)
+            
+    # Longest path computing process            
+    while (not zerodegreeq.isempty()):
+        # Remove one vertex from queue which have zero degree vertices and reduce the indegree by 1
+        curr_vertex = zerodegreeq.dequeue()
+        indegree[curr_vertex] = indegree[curr_vertex] - 1
+        
+        # Repeat for each adjacent of the removed vertex 
+        for adj_vertex in AList[curr_vertex]:
+            # Reduce the indegree of each adjacent of the removed vertex by 1
+            indegree[adj_vertex] = indegree[adj_vertex] - 1
+            # Assign the longest path value for each adjacent of the removed vertex
+            lpath[adj_vertex] = max(lpath[adj_vertex],lpath[curr_vertex]+1)
+            # If after reducing the degree of adjacent, it becomes zero then insert it into the queue
+            if indegree[adj_vertex] == 0:
+                zerodegreeq.enqueue(adj_vertex)
+                
+    return(lpath)
 
 
 
